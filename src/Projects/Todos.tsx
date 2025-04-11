0c0727f4a8897247e507e1e9b044cf7edd271db9
seraprogrammer <programmernazmul1@gmail.com>
2025-04-12 00:24:19 +0600

    Code cleanup

---
+++ src/Projects/Todos.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Todos() {
+  const [todos, setTodos] = useState([]);
+  
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+}
