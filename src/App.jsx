ed422c4922e98408d8008efd8fc7f4e2d8b5e61d
seraprogrammer <programmernazmul1@gmail.com>
2025-04-18 15:26:03 +0600

    Code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
-import { h, signal, effect } from "./olova.js";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
-  const count = signal(0);
-
-  effect(() => {
-    console.log(count());
-  });
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>{() => count()}</h1>
-      <button onclick={() => count.set(count() + 1)}>Click me!</button>
-
-      <p>This is inside the Helper component</p>
-
-      {/* SVG element example */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-air-vent-icon lucide-air-vent"
-      >
-        <path d="M6 12H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2" />
-        <path d="M6 8h12" />
-        <path d="M18.3 17.7a2.5 2.5 0 0 1-3.16 3.83 2.53 2.53 0 0 1-1.14-2V12" />
-        <path d="M6.6 15.6A2 2 0 1 0 10 17v-5" />
-      </svg>
-
-      {/* Another content */}
-      <button>Another button inside Helper</button>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
-import { h } from "./olova";
-import App from "./app.jsx";
-document.getElementById("root").appendChild(<App />);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
